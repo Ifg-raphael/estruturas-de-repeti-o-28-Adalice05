944b4a579cfd1310d048500ffdae3289e6247a17
Adalice <adalicebastos32@gmail.com>
2025-09-01 19:05:50 -0300

    Implementação final

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,13 @@
-# Sua solução aqui
+#Exercício 4
+
+#Entrada de dados
+
+n1= float(input("Insira o primeiro número: "))
+n2= float(input("Insira o segundo número: "))
+n3= float(input("Insira o terceiro número: "))
+n4= float(input("Insira o quarto número: "))
+n5= float(input("Insira o quinto número: "))
+
+media= (n1+n2+n3+n4+n5)/5 #Calcula a média aritemética dos números inseridos pelo usuário
+
+print(f"{media:.2f}") #Exibe o valor calculado
